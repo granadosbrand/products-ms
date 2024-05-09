3c6b00cac35801ef6fc263e4f2d06bd2f118c747
granadosbrand <alejandrogb189@gmail.com>
2024-05-09 17:27:04 -0500

    s5-46

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -10,26 +10,26 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   // @Post()
-  @MessagePattern({ cmd: 'create' })
+  @MessagePattern({ cmd: 'create_product' })
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   // @Get()
-  @MessagePattern({ cmd: 'find_all' })
+  @MessagePattern({ cmd: 'find_all_products' })
   findAll(@Payload() paginationDTO: PaginationDTO) {
     return this.productsService.findAll(paginationDTO);
   }
 
   // @Get(':id')
-  @MessagePattern({ cmd: 'find_one' })
+  @MessagePattern({ cmd: 'find_one_product' })
   findOne(@Payload('id', ParseIntPipe) id: number) {
     // { id: '1' }
     return this.productsService.findOne(+id);
   }
 
   // @Patch(':id')
-  @MessagePattern({ cmd: 'update' })
+  @MessagePattern({ cmd: 'update_product' })
   update(
     // @Param('id', ParseIntPipe) id: number,
     // @Body() updateProductDto: UpdateProductDto,
@@ -39,7 +39,7 @@ export class ProductsController {
   }
 
   // @Delete(':id')
-  @MessagePattern({ cmd: 'delete' })
+  @MessagePattern({ cmd: 'delete_product' })
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
