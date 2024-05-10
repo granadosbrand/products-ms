f9248f0b2d6b7d5adb536b93edcb3f63049c6686
Diego Granados <alejadrogb189@gmail.com>
2024-05-09 23:55:54 -0500

    s5-46

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -8,6 +8,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDTO } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -50,7 +51,7 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if (!resProduct) {
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new RpcException(`Product with id # ${id} not found`);
     }
 
     return {
